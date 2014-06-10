d0d06fe78af4322add02fd24d911f06187560adc
Ryuinferno <ryuinferno.xda@gmail.com>
2014-06-10 20:34:52 +1000

    Revert "Always show the action bar overflow"
    This reverts commit 71f0d85ef1eff0bf578b8717c0ad06e4bbd9bb35.
    
    Conflicts:
    	src/com/android/browser/NavScreen.java
    
    Change-Id: Iea303807ae36eecdcb8c946730598871218ffc8b

--- src/com/android/browser/NavScreen.java
+++ src/com/android/browser/NavScreen.java
@@ -71,6 +71,7 @@ public class NavScreen extends RelativeLayout
     NavTabScroller mScroller;
     TabAdapter mAdapter;
     int mOrientation;
+    boolean mNeedsMenu;
     HashMap<Tab, View> mTabViews;
 
     public NavScreen(Activity activity, UiController ctl, PhoneUi ui) {
@@ -146,6 +147,10 @@ public class NavScreen extends RelativeLayout
                 onCloseTab(tab);
             }
         });
+        mNeedsMenu = !ViewConfiguration.get(getContext()).hasPermanentMenuKey();
+        if (!mNeedsMenu) {
+            mMore.setVisibility(View.GONE);
+        }
     }
 
     @Override
