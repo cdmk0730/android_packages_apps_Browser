27f220b51f68d994e27775fc647f8f53a2814591
martincz <martincz@mokeedev.com>
2014-11-20 22:28:37 +0800

    Browser: support exit program
    Change-Id: I83041438c6f563b699aebba515008aa24479c1e9

--- src/com/android/browser/Controller.java
+++ src/com/android/browser/Controller.java
@@ -1617,6 +1617,10 @@ public class Controller
                 findOnPage();
                 break;
 
+            case R.id.exit_menu_id:
+                mActivity.finish();
+                break;
+
             case R.id.page_info_menu_id:
                 showPageInfo();
                 break;
